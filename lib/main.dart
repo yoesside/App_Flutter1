e14b6d40d656d4a2dc7ccd3358172ba47fc405d5
yogaerasubakti <yogaera29@gmail.com>
2020-10-12 17:08:42 +0800

    FlutterStatelessWidget

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int number = 0;
+
+  void tekanTombol() {
+    
+    setState(() {//mengubah tampilan sesuai dengan state yang ada saat ini
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful widget demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //children: <Widget>[
+            Text(number.toString(), style: TextStyle(fontSize: number.toDouble()),),
+            RaisedButton(
+              child: Text("Tambah bilangan"),
+              onPressed: tekanTombol,
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
